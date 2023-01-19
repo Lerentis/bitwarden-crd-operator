6a324e66da622ea608119cbdf52733eecfcb2abe
Thibault Cohen <47721+titilambert@users.noreply.github.com>
2023-01-18 20:57:21 -0500

    Unlock vault only when it's needed

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,4 +1,5 @@
 import os
+import json
 import subprocess
 
 class BitwardenCommandException(Exception):
@@ -8,6 +9,11 @@ def get_secret_from_bitwarden(id):
     return command_wrapper(command=f"get item {id}")
 
 def unlock_bw(logger):
+    status_output = command_wrapper("status")
+    status = json.loads(status_output)['status']
+    if status == 'unlocked':
+        logger.info("Already unlocked")
+        return
     token_output = command_wrapper("unlock --passwordenv BW_PASSWORD")
     tokens = token_output.split('"')[1::2]
     os.environ["BW_SESSION"] = tokens[1]
