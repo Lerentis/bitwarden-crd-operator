5b445ae668ed89342b1374d355cc805d399f922b
Tobias Trabelsi <lerentis@uploadfilter24.eu>
2024-06-23 00:33:29 +0200

    still wip

--- utils/test.sh
+++ utils/test.sh
@@ -1,4 +1,28 @@
 #!/bin/bash
 
-kubectl apply -f example*
+for f in /*.yaml
+do
+  echo "applying $f ..."
+  kubectl apply -f "$f"
+done
 
+kubectl get secret test-regcred
+if [ $? -ne 0 ]; then
+    echo "Could not find secret test-regcred"
+    exit 1
+fi
+kubectl get secret test-scope
+if [ $? -ne 0 ]; then
+    echo "Could not find secret test-scope"
+    exit 1
+fi
+kubectl get secret test-secret
+if [ $? -ne 0 ]; then
+    echo "Could not find secret test-secret"
+    exit 1
+fi
+kubectl get secret test-template
+if [ $? -ne 0 ]; then
+    echo "Could not find secret test-template"
+    exit 1
+fi
